0491f506585e7e11a46e931f6e8ab6721fc1a64e
venkatesh battula <battulavenkatesh2169@gmail.com>
2024-03-26 17:03:38 +0530

    adminlte-3 added

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    // $.widget.bridge('uibutton', $.ui.button);
+  }
+
+}
