ac698ce40a6df5e332d74a504c160a62a488d71f
venkatesh battula <battulavenkatesh2169@gmail.com>
2024-03-27 12:13:34 +0530

    jewellery-template-practice added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { SectionComponent } from './components/section/section.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component:HomeComponent,
+    children:[
+      {
+        path:"",
+        component:SectionComponent
+      },
+      {
+        path:"product",
+        component:ProductComponent
+      },
+      {
+        path:"product-detail",
+        component:ProductDetailComponent
+      },
+      {
+        path:"blog",
+        component:BlogComponent
+      },
+      {
+        path:"blog-detail",
+        component:BlogDetailComponent
+      },
+      {
+        path:"about-us",
+        component:AboutUsComponent
+      },
+      {
+        path:"contact",
+        component:ContactComponent
+      },
+      {
+        path:"login",
+        component:LoginComponent
+      },
+      {
+        path:"cart",
+        component:CartComponent
+      },
+      {
+        path:"checkout",
+        component:CheckoutComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SectionComponent } from './components/section/section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    BlogComponent,
+    BlogDetailComponent,
+    AboutUsComponent,
+    ContactComponent,
+    LoginComponent,
+    CartComponent,
+    CheckoutComponent,
+    DropdownComponent,
+    HeaderComponent,
+    FooterComponent,
+    SectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
