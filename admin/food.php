5b20d3b46e13bf10962e8ed9a042ed446e47fdba
venkatesh battula <battulavenkatesh2169@gmail.com>
2024-03-27 17:05:22 +0530

    bootstrap,css,html files added

---
+++ admin/food.php
@@ -0,0 +1,119 @@
+<html>
+    <head>
+        <title> level-1</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!--https://fontawesome.com/v4/icons/-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+                        <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Logo</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="d-flex">
+                            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                                <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link me-2" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>    
+                                </ul>
+                            </div>
+                        </div>
+                  </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+
+                    <div class="list-group">
+                        <a href="index.html" class="list-group-item list-group-item-action"> Restaurant</a>
+                        <a href="level1.html" class="list-group-item list-group-item-action  "> Level-1 categories</a>
+                        <a href="level2.html" class="list-group-item list-group-item-action  "> Level-2 categories</a>
+                        <a href="food.html" class="list-group-item list-group-item-action active"> Food items</a>
+                    </div>
+                </div>
+                <div class="col-10">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>
+                               Food items 
+                            </h3>
+                            <div>
+                            <>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered   table-hover">
+                                  <thead class="table-info">
+                                    <tr>
+                                      <th>SNo</th>
+                                      <th> Item Name</th>
+                                      <th>  Original price</th>
+                                      <th>  Discount</th>
+                                      <th>  selling price</th>
+                                      <th> Status</th>
+                                      <th> Delete</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <td>1</td>
+                                      <td>chicken biryani</td>
+                                      <td>  450</td>
+                                      <td>5%</td>
+                                      <td> 400  </td>
+                                      <td> Open</td>
+                                      <td> <i class="fa fa-trash-o"></i></td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>mutton biryani</td>
+                                      <td>  450</td>
+                                      <td>5%</td>
+                                      <td> 400  </td>
+                                      <td> Open</td>
+                                      <td> <i class="fa fa-trash-o"></i></td>
+                                      </tr>
+                                      <tr>
+                                        <td>3</td>
+                                        <td>prawns biryani</td>
+                                      <td>  450</td>
+                                      <td>5%</td>
+                                      <td> 400  </td>
+                                      <td> Open</td>
+                                      <td> <i class="fa fa-trash-o"></i></td>
+                                      </tr>
+                                      <tr>
+                                        <td>4</td>
+                                        <td> fish biryani</td>
+                                      <td>  450</td>
+                                      <td>5%</td>
+                                      <td> 400  </td>
+                                      <td> Open</td>
+                                      <td> <i class="fa fa-trash-o"></i></td>
+                                      </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>     
+    </body>
+</html>
