a9a86467c2c4f1090780e50dbfe0f56a71f766f4
venkatesh battula <battulavenkatesh2169@gmail.com>
2024-03-27 11:59:22 +0530

    store-template added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SectionComponent } from './components/section/section.component';
+import { ProductComponent } from './components/product/product.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent,
+    children:[
+      {
+        path:"",
+        component:SectionComponent
+      },
+      {
+        path:"product",
+        component:ProductComponent
+      },
+      {
+        path:"detail",
+        component:DetailComponent
+      },
+      {
+        path:"about-us",
+        component:AboutUsComponent
+      },
+      {
+        path:"contact",
+        component:ContactComponent
+      },
+      {
+        path:"login",
+        component:LoginComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
