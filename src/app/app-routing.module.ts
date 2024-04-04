38b9b212903e84ea1837ef665b5f228d1d7c259f
venkatesh <battulavenkatesh2169@gmail.com>
2024-04-04 10:46:01 +0530

    moviee-template photos added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SectionComponent } from './components/section/section.component';
-import { ArticleComponent } from './components/article/article.component';
-import { OfferComponent } from './components/offer/offer.component';
-import { TypographyComponent } from './components/typography/typography.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
+import { LoginComponent } from './components/login/login.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { TeamComponent } from './components/team/team.component';
+import { AccountComponent } from './components/account/account.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
 
 const routes: Routes = [
   {
@@ -17,20 +21,32 @@ const routes: Routes = [
         component:SectionComponent
       },
       {
-        path:"article",
-        component:ArticleComponent
+        path:"about",
+        component:AboutComponent
       },
       {
-        path:"offer",
-        component:OfferComponent
+        path:"blog",
+        component:BlogComponent
       },
       {
-        path:"typography",
-        component:TypographyComponent
+        path:"blog-detail",
+        component:BlogDetailComponent
+      },
+      {
+        path:"login",
+        component:LoginComponent
       },
       {
         path:"contact",
         component:ContactComponent
+      },
+      {
+        path:"team",
+        component:TeamComponent
+      },
+      {
+        path:"account",
+        component:AccountComponent
       }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TeamComponent } from './components/team/team.component';
+import { AccountComponent } from './components/account/account.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SectionComponent } from './components/section/section.component';
-import { ArticleComponent } from './components/article/article.component';
-import { OfferComponent } from './components/offer/offer.component';
-import { TypographyComponent } from './components/typography/typography.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    BlogDetailComponent,
+    LoginComponent,
+    ContactComponent,
+    TeamComponent,
+    AccountComponent,
     HeaderComponent,
     FooterComponent,
     SectionComponent,
-    ArticleComponent,
-    OfferComponent,
-    TypographyComponent,
-    ContactComponent
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
