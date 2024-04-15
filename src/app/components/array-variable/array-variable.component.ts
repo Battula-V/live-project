c6f3615f5b1c105b5824e81c993427edf73913c0
venkatesh <battulavenkatesh2169@gmail.com>
2024-04-15 17:48:40 +0530

    arrays and objects updated

--- src/app/components/array-variable/array-variable.component.ts
+++ src/app/components/array-variable/array-variable.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./array-variable.component.scss']
 })
 export class ArrayVariableComponent implements OnInit{
-  myFriends:Array< string> = ["somu", "ramu"];
+  myFriends:Array<string> = ["somu", "ramu"];
   // To store any data type value in the array
-  friends:Array< any> = ["somu", "ramu", 2];
+  friends:Array<any> = ["somu", "ramu", 2];
   ngOnInit(): void {
     
   }
