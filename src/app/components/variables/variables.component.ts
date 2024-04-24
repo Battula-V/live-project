d205995547a400ec7dea9a944f71afdaa66fb553
venkatesh <battulavenkatesh2169@gmail.com>
2024-04-24 16:03:18 +0530

    variables added

--- src/app/components/variables/variables.component.ts
+++ src/app/components/variables/variables.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-variables',
   templateUrl: './variables.component.html',
   styleUrls: ['./variables.component.scss']
 })
-export class VariablesComponent {
+export class VariablesComponent implements OnInit{
+  address:string="guntur";
+  rollNumber:number=145;
+  is_city:boolean=true;
+  are_students:boolean=false;
+  item_price:number=23.9;
+  ngOnInit(): void {
+    let userName="venkatesh";
+    console.log(userName);
+    console.log(userName);
+    console.log(userName);
+    console.log(userName);
+    console.log(userName);
+  }
 
 }
