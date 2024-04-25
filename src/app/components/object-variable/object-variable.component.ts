45055ef02678d00450b40fb8faf2d465483ecb90
venkatesh <battulavenkatesh2169@gmail.com>
2024-04-25 15:09:29 +0530

    subject added

--- src/app/components/object-variable/object-variable.component.ts
+++ src/app/components/object-variable/object-variable.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { DataSharingService } from "../data-sharing/data-sharing.service";
 @Component({
   selector: 'app-object-variable',
   templateUrl: './object-variable.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ObjectVariableComponent {
   myFriends={ friend2:"somu" , friend3:"akbar" ,friend1:"ramu"}
+  constructor(private dataSharingServiceObj : DataSharingService){
+      this.dataSharingServiceObj.applicationData.subscribe(
+        ( va:any) => {
+          console.log(va);
+        }
+      );
+  }
+
 }
