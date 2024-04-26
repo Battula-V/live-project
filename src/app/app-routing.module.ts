8f8d8972634bac91bdd6324177fa10b551c2e68b
venkatesh <battulavenkatesh2169@gmail.com>
2024-04-26 22:46:57 +0530

    type two added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -56,7 +56,7 @@ const routes: Routes = [
         component:DataSharingTypeTwoComponent
       },
       {
-        path:"type-three",
+        path:"type-three/:name",
         component:DataSharingTypeThreeComponent
       },
       {
--- src/app/components/data-sharing/components/data-sharing-type-three/data-sharing-type-three.component.ts
+++ src/app/components/data-sharing/components/data-sharing-type-three/data-sharing-type-three.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 @Component({
   selector: 'app-data-sharing-type-three',
   templateUrl: './data-sharing-type-three.component.html',
   styleUrls: ['./data-sharing-type-three.component.scss']
 })
-export class DataSharingTypeThreeComponent {
-
+export class DataSharingTypeThreeComponent implements OnInit{
+      constructor(private activatedRouteObj : ActivatedRoute , private routerObj : Router){
+          activatedRouteObj.queryParams.subscribe(
+            (venky : any) => {
+              console.log(venky);
+            }
+          );
+      }
+  ngOnInit(): void {
+    this.activatedRouteObj.params.subscribe(
+      (venky : any) => {
+        console.log(venky);
+      }
+    );
+  }
 }
