e35d27815f4f76568fcfa6afa05ab28363f0d5e0
venkatesh <battulavenkatesh2169@gmail.com>
2024-05-01 17:54:06 +0530

    menu-bar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path : "online-exams",
-    loadChildren : () => import("./modules/online-exams/online-exams.module").then((m) => m.OnlineExamsModule)
+    loadChildren : () => import("./modules/online-exam/online-exam.module").then((m) => m.OnlineExamModule)
   },
   {
     path : "online-shopping",
     loadChildren : () => import("./modules/online-shopping/online-shopping.module").then((m) => m.OnlineShoppingModule)
   },
   {
-    path : "**",
+    path :"**",
     redirectTo : "online-exams"
   }
 ]
---
+++ src/app/modules/online-exam/components/jntu-exam/jntu-exam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jntu-exam',
+  templateUrl: './jntu-exam.component.html',
+  styleUrls: ['./jntu-exam.component.scss']
+})
+export class JntuExamComponent {
+
+}
